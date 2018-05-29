882f60b485b5e2a836f6ceea508682b3b32b1e1c
Andrey Miskov <amiskov@oggettoweb.com>
2018-05-30 01:45:15 +0300

    Simplify directory structure

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # if you are deploying to a custom domain
 echo 'andreymiskov.ru' > CNAME
@@ -15,10 +15,6 @@ echo 'andreymiskov.ru' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-# sleep 3
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:amiskov/mooc_programming-languages.git master:gh-pages
